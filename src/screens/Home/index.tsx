3cc6ee4a8c82de58c899f8d5ac9f4e101abfc559
Marcos <mguibtech@gmail.com>
2022-08-26 13:35:26 -0400

    Criando tela de home e criando formulário de adição de participante

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,40 @@
+import { View, Text, TextInput, TouchableOpacity, Alert} from "react-native";
+
+import {styles} from './styles';
+
+export function Home(){
+
+    function handleParticipantAdd(){
+        console.log('Clicou no botão de adicionar participante');
+
+        Alert.alert("Você clicou", "Este é um botão de add participante")
+    }
+
+  return(
+    <View style={styles.container}>
+        <Text style={styles.eventName}>
+            Nome do evento
+        </Text>
+        <Text style={styles.eventDate}>
+            Sexta, 26 de agosto
+        </Text>
+
+        <View style={styles.form}>
+            <TextInput
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor='#6b6b8b'
+            />
+
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={handleParticipantAdd}
+            >
+                <Text style={styles.buttonText}>
+                    +
+                </Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    },
+    eventName:{
+      color:'#ffff',
+      fontSize: 24,
+      fontWeight:'bold'
+    },
+    eventDate:{
+      color:'#6b6b6b',
+      fontSize: 16,
+    },
+    input:{
+        flex: 1,
+        backgroundColor: '#1f1e25',
+        height:56,
+        borderRadius: 5,
+        color: '#fdfcfe',
+        padding:16,
+        fontSize: 16,
+        marginRight: 12
+    },
+    buttonText:{
+        color: '#fff',
+        fontSize: 24,
+    },
+    button:{
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31cf67',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    form:{
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42
+    }
+  })
